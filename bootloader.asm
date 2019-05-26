614cdc26505ec4190811aa590aaecd79b15d2971
Jack O'Sullivan <jackos1998@gmail.com>
2019-05-26 13:19:34 +0100

    Initial commit

---
+++ bootloader.asm
@@ -0,0 +1,285 @@
+; Minimal Linux Bootloader
+; ========================
+
+; @ author:	Sebastian Plotz
+; @ version:	1.0
+; @ date:	24.07.2012
+
+; Copyright (C) 2012 Sebastian Plotz
+
+; Minimal Linux Bootloader is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; Minimal Linux Bootloader is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with Minimal Linux Bootloader. If not, see <http://www.gnu.org/licenses/>.
+
+; Memory layout
+; =============
+
+; 0x07c00 - 0x07dff	Mininal Linux Bootloader
+;			+ partition table
+;			+ MBR signature
+; 0x10000 - 0x17fff	Real mode kernel
+; 0x18000 - 0x1dfff	Stack and heap
+; 0x1e000 - 0x1ffff	Kernel command line
+; 0x20000 - 0x2fdff	temporal space for
+;			protected-mode kernel
+; 0x30000 - extra boot code
+
+; base_ptr = 0x10000
+; heap_end = 0xe000
+; heap_end_ptr = heap_end - 0x200 = 0xde00
+; cmd_line_ptr = base_ptr + heap_end = 0x1e000
+
+org	0x7c00
+
+	cli
+	xor	ax, ax
+	mov	ds, ax
+	mov	ss, ax
+	mov	sp, 0x7c00			; setup stack ...
+	mov	ax, 0x1000
+	mov	es, ax
+	sti
+
+read_extra_code:
+	mov	eax, 0x0001			; load one sector
+	xor	bx, bx				; no offset
+	mov	cx, 0x7e0			; load extra code after bootsector (read by bios)
+	call	read_from_hdd
+
+	mov dword [current_lba], kernel_lba
+
+read_kernel_bootsector:
+
+	mov	eax, 0x0001			; load one sector
+	xor	bx, bx				; no offset
+	mov	cx, 0x1000			; load Kernel boot sector at 0x10000
+	call	read_from_hdd
+
+read_kernel_setup:
+
+	xor	eax, eax
+	mov	al, [es:0x1f1]			; no. of sectors to load
+	cmp	ax, 0				; 4 if setup_sects = 0
+	jne	read_kernel_setup.next
+	mov	ax, 4
+.next:
+	mov	bx, 512				; 512 byte offset
+	mov	cx, 0x1000
+	call	read_from_hdd
+
+check_version:
+
+	cmp dword [es:0x202], 'HdrS'
+	jb	error
+	cmp	word [es:0x206], 0x204		; we need protocol version >= 2.04
+	jb	error
+	test	byte [es:0x211], 1
+	jz	error
+
+set_header_fields:
+
+	mov	byte [es:0x210], 0xe1		; set type_of_loader
+	or	byte [es:0x211], 0x80		; set CAN_USE_HEAP
+	mov	dword [es:0x218], 0			; set ramdisk_image
+	mov	dword [es:0x21c], 0			; set ramdisk_size
+	mov	word [es:0x224], 0xde00		; set heap_end_ptr
+	;mov	byte [es:0x226], 0x00		; set ext_loader_ver
+	mov	byte [es:0x227], 0x01		; set ext_loader_type (bootloader id: 0x11)
+	mov	dword [es:0x228], 0x1e000	; set cmd_line_ptr
+	cld					; copy cmd_line
+	mov	si, cmd_line
+	mov	di, 0xe000
+	mov	cx, cmd_length
+	rep	movsb
+
+read_protected_mode_kernel:
+
+	mov	edx, [es:0x1f4]			; edx stores the number of bytes to load
+	shl	edx, 4
+.loop:
+	cmp	edx, 0
+	je	run_kernel
+	cmp	edx, 0xfe00			; less than 127*512 bytes remaining?
+	jb	read_protected_mode_kernel_2
+	mov	eax, 0x7f			; load 127 sectors (maximum)
+	xor	bx, bx				; no offset
+	mov	cx, 0x2000			; load temporary to 0x20000
+	call	read_from_hdd
+	mov	cx, 0x7f00			; move 65024 bytes (127*512 byte)
+	call	do_move
+	sub	edx, 0xfe00			; update the number of bytes to load
+	add	word [gdt.dest], 0xfe00
+	adc	byte [gdt.dest+2], 0
+	jmp	short read_protected_mode_kernel.loop
+
+read_protected_mode_kernel_2:
+
+	mov	eax, edx
+	shr	eax, 9
+	test	edx, 511
+	jz	read_protected_mode_kernel_2.next
+	inc	eax
+.next:
+	xor	bx, bx
+	mov	cx, 0x2000
+	call	read_from_hdd
+	mov	ecx, edx
+	shr	ecx, 1
+	call	do_move
+
+run_kernel:
+
+	cli
+	mov	ax, 0x1000
+	mov	ds, ax
+	mov	es, ax
+	mov	fs, ax
+	mov	gs, ax
+	mov	ss, ax
+	mov	sp, 0xe000
+	jmp	0x1020:0
+
+read_from_hdd:
+
+	push	edx
+	mov	[dap.count], ax
+	mov	[dap.offset], bx
+	mov	[dap.segment], cx
+	mov	edx, [current_lba]
+	mov	[dap.lba], edx
+	add	[current_lba], eax		; update current_lba
+	mov	ah, 0x42
+	mov	si, dap
+	mov	dl, 0x80			; first hard disk
+	int	0x13
+	jc	error
+	pop	edx
+	ret
+
+
+; Global Descriptor Table
+
+gdt:
+
+	times	16	db	0
+	dw	0xffff				; segment limit
+.src:
+	dw	0
+	db	2
+	db	0x93				; data access rights
+	dw	0
+	dw	0xffff				; segment limit
+.dest:
+	dw	0
+	db	0x10				; load protected-mode kernel to 100000h
+	db	0x93				; data access rights
+	dw	0
+	times	16	db	0
+
+; Disk Address Packet
+
+dap:
+
+	db	0x10				; size of DAP
+	db	0				; unused
+.count:
+	dw	0				; number of sectors
+.offset:
+	dw	0				; destination: offset
+.segment:
+	dw	0				; destination: segment
+.lba:
+	dd	0				; low bytes of LBA address
+	dd	0				; high bytes of LBA address
+
+current_lba	dd	1			; initialize to first LBA address
+
+	times	440-($-$$)	db	0
+
+	; nt "disk signature"
+	dd 0xcafebabe
+	dw 0
+
+	; partition no 1
+	db 0		; inactive
+	; chs first sector (to be filled)
+	db 0		; head
+	db 0		; (bits 7-6 are cylinder, 5-0 are sector)
+	db 0		; rest of cylinder
+	db 0x1		; partition type ("FAT12")
+	; chs last sector (to be filled)
+	db 0		; head
+	db 0		; (bits 7-6 are cylinder, 5-0 are sector)
+	db 0		; rest of cylinder
+	dd 0		; lba first sector (to be filled)
+	dd 0		; number of sectors (to be filled)
+
+	; partition no 2
+	db 0		; inactive
+	; chs first sector (to be filled)
+	db 0		; head
+	db 0		; (bits 7-6 are cylinder, 5-0 are sector)
+	db 0		; rest of cylinder
+	db 0x83		; partition type ("Linux filesystem")
+	; chs last sector (to be filled)
+	db 0		; head
+	db 0		; (bits 7-6 are cylinder, 5-0 are sector)
+	db 0		; rest of cylinder
+	dd 0		; lba first sector (to be filled)
+	dd 0		; number of sectors (to be filled)
+
+	times 16 db 0
+	times 16 db 0
+
+	; mbr magic
+	dw	0xaa55
+
+; this code won't be loaded by the bios, so we'll have to load it into ram ourselves
+extra_code:
+do_move:
+
+	push	edx
+	push	es
+	xor	ax, ax
+	mov	es, ax
+	mov	ah, 0x87
+	mov	si, gdt
+	int	0x15
+	jc	error
+	pop	es
+	pop	edx
+	ret
+
+error:
+
+	mov	si, error_msg
+
+msg_loop:
+
+	lodsb
+	and	al, al
+	jz	reboot
+	mov	ah, 0xe
+	mov	bx, 7
+	int	0x10
+	jmp	short msg_loop
+
+reboot:
+
+	xor	ax, ax
+	int	0x16
+	int	0x19
+	jmp	0xf000:0xfff0			; BIOS reset code
+
+cmd_line	db	'', 0
+cmd_length	equ	$ - cmd_line
+error_msg	db	'CVOS bootloader error', 0xd, 0xa, 0
---
+++ mk_disk.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import os
+from os import path
+import sys
+import struct
+from subprocess import check_call, check_output
+import tempfile
+import shutil
+
+BL_SIZE = 585
+
+def sector_aligned(n):
+    if n % 512 == 0:
+        return n
+    return n + (512 - (n % 512))
+
+PTABLE = 446
+def pentry(n):
+    return PTABLE + ((n-1)*16)
+
+FIRST_SECTOR     = 0x8
+SECTOR_COUNT     = 0xc
+CHS_FIRST_SECTOR = 0x1
+CHS_LAST_SECTOR  = 0x5
+
+HEADS_PER_CYLINDER = 255
+SECTORS_PER_TRACK = 63
+
+CYLINDER_HI_MASK = 0b1100000000
+def lba_to_chs(lba):
+    cylinder = lba // (HEADS_PER_CYLINDER * SECTORS_PER_TRACK)
+    head = (lba // SECTORS_PER_TRACK) % HEADS_PER_CYLINDER
+    sector = (lba % SECTORS_PER_TRACK) + 1
+
+    print(f'lba {lba} -> cylinder {cylinder}, head {head}, sector {sector}')
+    return struct.pack('B', head) + \
+        struct.pack('B', ((cylinder & CYLINDER_HI_MASK) >> 2) | (sector & 0xff)) + \
+        struct.pack('B', cylinder & 0xff)
+
+def write_psize(mbr, pnum, start, size):
+    b = pentry(pnum)
+    last_sector = start + size - 1
+
+    mbr.seek(b+FIRST_SECTOR, os.SEEK_SET)
+    mbr.write(struct.pack('<I', start))
+    mbr.seek(b+SECTOR_COUNT, os.SEEK_SET)
+    mbr.write(struct.pack('<I', size))
+
+    mbr.seek(b+CHS_FIRST_SECTOR, os.SEEK_SET)
+    mbr.write(lba_to_chs(start))
+    mbr.seek(b+CHS_LAST_SECTOR, os.SEEK_SET)
+    mbr.write(lba_to_chs(last_sector))
+
+def get_fat_file_offset(fs, f):
+    output = check_output(['fatcat', '-l', path.dirname(f), fs], encoding='utf8').split('\n')
+    for line in output:
+        words = list(filter(lambda w: len(w) != 0, line.split(' ')))
+        if words[0] == 'f' and words[3] == path.basename(f):
+            cluster = words[4].split('=')[1]
+            break
+
+    if not cluster:
+        raise Exception(f'{f} not in {fs}')
+
+    output = check_output(['fatcat', '-@', cluster, fs], encoding='utf8').split('\n')
+    offset = int(output[1].split(' ')[0])
+    print(f'{f} starts at cluster {cluster} ({offset} bytes) on {fs}')
+    return offset
+
+def main():
+    if len(sys.argv) != 6:
+        print(f'usage: {sys.argv[0]} <bootloader source> <pdf> <kernel> <rootfs> <output image>')
+        sys.exit(1)
+
+    bl_source = sys.argv[1]
+    pdf_file = sys.argv[2]
+    kernel_file = sys.argv[3]
+    rootfs_file = sys.argv[4]
+    out_file = sys.argv[5]
+    with open(out_file, 'wb') as disk:
+        with open(pdf_file, 'rb') as pdf:
+            pdf.seek(0, os.SEEK_END)
+            pdf_size = BL_SIZE + pdf.tell()
+            aligned_pdf = sector_aligned(pdf_size)
+            print(f'bootloader + pdf size: {pdf_size} bytes (aligned to {aligned_pdf} bytes) - {aligned_pdf // 512} sectors')
+
+            esp_start = aligned_pdf // 512
+
+            kernel_size = path.getsize(kernel_file)
+            print(f'kernel size: {kernel_size} bytes')
+            esp_size = sector_aligned(kernel_size) + (64*1024)
+            assert esp_size % 512 == 0
+            print(f'creating fat12 filesystem of size {esp_size} ({esp_size/512} sectors)')
+            with tempfile.NamedTemporaryFile('w+b', prefix='esp-', suffix='.img') as esp:
+                esp.truncate(esp_size)
+                check_call(['mkfs.fat', '-F12', '-n', 'ESP', esp.name])
+                with tempfile.TemporaryDirectory(prefix='esp-') as esp_mount:
+                    check_call(['sudo', 'mount', '-o', f'uid={os.geteuid()},gid={os.getegid()}', esp.name, esp_mount])
+                    boot_dir = path.join(esp_mount, 'efi', 'boot')
+                    os.makedirs(boot_dir)
+                    boot_file = path.join(boot_dir, 'bootx64.efi')
+                    shutil.copyfile(kernel_file, boot_file)
+                    check_call(['sudo', 'umount', esp_mount])
+
+                kernel_offset = get_fat_file_offset(esp.name, '/efi/boot/bootx64.efi')
+
+                assert kernel_offset % 512 == 0
+                kernel_lba = esp_start + (kernel_offset // 512)
+                rfs_lba = esp_start + (esp_size // 512)
+                print(f'esp lba: {esp_start}, kernel lba: {kernel_lba}, rootfs lba: {rfs_lba}')
+
+                with open(rootfs_file, 'rb') as rootfs:
+                    rootfs.seek(0, os.SEEK_END)
+                    rfs_size = rootfs.tell()
+                    rfs_sectors = sector_aligned(rfs_size) // 512
+                    print(f'rootfs is {rfs_size} bytes ({rfs_sectors} sectors)')
+                    with tempfile.NamedTemporaryFile('w+b', prefix='boot-', suffix='.bin') as bootloader:
+                        check_call(['nasm', '-D', f'kernel_lba={kernel_lba}', '-o', bootloader.name, bl_source])
+                        bootloader.seek(0, os.SEEK_END)
+                        assert bootloader.tell() == BL_SIZE
+
+                        write_psize(bootloader, 1, esp_start, esp_size // 512)
+                        write_psize(bootloader, 2, rfs_lba, rfs_sectors)
+
+                        bootloader.seek(0, os.SEEK_SET)
+                        disk.write(bootloader.read())
+
+                    pdf.seek(0, os.SEEK_SET)
+                    while True:
+                        data = pdf.read(512)
+                        if not data:
+                            break
+                        disk.write(data)
+                    if pdf_size != aligned_pdf:
+                        padding = aligned_pdf - pdf_size
+                        print(f'padding pdf by {padding} bytes')
+                        disk.seek(padding, os.SEEK_CUR)
+
+                    while True:
+                        data = esp.read(512)
+                        if not data:
+                            break
+                        disk.write(data)
+
+                    rootfs.seek(0, os.SEEK_SET)
+                    while True:
+                        data = rootfs.read(512)
+                        if not data:
+                            break
+                        disk.write(data)
+
+        disk_size = disk.tell()
+        disk_aligned = sector_aligned(disk_size)
+        if disk_size != disk_aligned:
+            print(f'aligning final disk ({disk_size} bytes) to sectors ({disk_aligned} bytes)')
+            disk.truncate(disk_aligned)
+
+if __name__ == '__main__':
+    main()
